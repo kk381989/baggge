895bf5df30b2f6e10a62a61144fb26e7f5a8f7db
19ankit94 <19ankit94@gmail.com>
2018-01-21 00:17:19 +0530

    feat:signup validation

--- public/js/validations.js
+++ public/js/validations.js
@@ -1,93 +1,70 @@
-$(document).ready(function(){
-        document.getElementById("menu1").style.backgroundColor = "white";
-  		document.getElementById("menu5").style.backgroundColor = "#4c50cd";
-        $("#electricity").hide();
-        $("#water").hide();
-        $("#dth").hide();
-        $("#bus").hide();
-        $("#hotels").hide();
-        $("#holiday_package").hide();
-    $("#menu1").click(function(){
-        document.getElementById("menu1").style.backgroundColor = "white";
-        document.getElementById("menu2").style.backgroundColor = "transparent";
-        document.getElementById("menu3").style.backgroundColor = "transparent";
-        document.getElementById("menu4").style.backgroundColor = "transparent";
-    	$("#mobile").show();
-        $("#electricity").hide();
-        $("#water").hide();
-        $("#dth").hide();
-    });
-    $("#menu2").click(function(){
-        document.getElementById("menu1").style.backgroundColor = "transparent";
-        document.getElementById("menu2").style.backgroundColor = "white";
-        document.getElementById("menu3").style.backgroundColor = "transparent";
-        document.getElementById("menu4").style.backgroundColor = "transparent";
-        $("#electricity").show();
-        $("#mobile").hide();
-        $("#water").hide();
-        $("#dth").hide();
-    });
-    $("#menu3").click(function(){
-    	document.getElementById("menu1").style.backgroundColor = "transparent";
-        document.getElementById("menu2").style.backgroundColor = "transparent";
-        document.getElementById("menu3").style.backgroundColor = "white";
-        document.getElementById("menu4").style.backgroundColor = "transparent";
-        $("#water").show();
-        $("#electricity").hide();
-        $("#mobile").hide();
-        $("#dth").hide();
-    });
-    $("#menu4").click(function(){
-    	document.getElementById("menu1").style.backgroundColor = "transparent";
-        document.getElementById("menu2").style.backgroundColor = "transparent";
-        document.getElementById("menu3").style.backgroundColor = "transparent";
-        document.getElementById("menu4").style.backgroundColor = "white";
-        $("#dth").show();
-        $("#mobile").hide();
-        $("#electricity").hide();
-        $("#water").hide();
-    });
-    $("#menu5").click(function(){
-        document.getElementById("menu5").style.backgroundColor = "#4c50cd";
-        document.getElementById("menu6").style.backgroundColor = "transparent";
-        document.getElementById("menu7").style.backgroundColor = "transparent";
-        document.getElementById("menu8").style.backgroundColor = "transparent";
-        $("#flight").show();
-        $("#bus").hide();
-        $("#hotels").hide();
-        $("#holiday_package").hide();
-    });
-    $("#menu6").click(function(){
-        document.getElementById("menu5").style.backgroundColor = "transparent";
-        document.getElementById("menu6").style.backgroundColor = "#4c50cd";
-        document.getElementById("menu7").style.backgroundColor = "transparent";
-        document.getElementById("menu8").style.backgroundColor = "transparent";
-        $("#bus").show();
-        $("#flight").hide();
-        $("#hotels").hide();
-        $("#holiday_package").hide();
-    });
-    $("#menu7").click(function(){
-        document.getElementById("menu5").style.backgroundColor = "transparent";
-        document.getElementById("menu6").style.backgroundColor = "transparent";
-        document.getElementById("menu7").style.backgroundColor = "#4c50cd";
-        document.getElementById("menu8").style.backgroundColor = "transparent";
-        $("#hotels").show();
-        $("#flight").hide();
-        $("#bus").hide();
-        $("#holiday_package").hide();
-    });
-    $("#menu8").click(function(){
-        document.getElementById("menu5").style.backgroundColor = "transparent";
-        document.getElementById("menu6").style.backgroundColor = "transparent";
-        document.getElementById("menu7").style.backgroundColor = "transparent";
-        document.getElementById("menu8").style.backgroundColor = "#4c50cd";
-        $("#holiday_package").show();
-        $("#flight").hide();
-        $("#bus").hide();
-        $("#hotels").hide();
-    });
-});
+// signUp form validations
+const userName = document.getElementById("userName");
+const emailId = document.getElementById("emailId");
+const mobileNumber  = document.getElementById("mobileNumber");
+const passWord = document.getElementById("passWord");
+const letter = document.getElementById("letter");
+const capital = document.getElementById("capital");
+const number = document.getElementById("number");
+const length = document.getElementById("length");
+const splChars = document.getElementById("splChars");
+
+
+// When the user clicks on the password field, show the message box
+passWord.onfocus = function() {
+  document.getElementById("message").style.display = "block";
+}
+
+// When the user clicks outside of the password field, hide the message box
+passWord.onblur = function() {
+  document.getElementById("message").style.display = "none";
+}
+
+passWord.onkeyup = function() {
+  const lowerCaseLetters = /[a-z]/g;
+  const upperCaseLetters = /[A-Z]/g;
+  const numbers = /[0-9]/g;
+  const sChars = "!`@#$%^&*()+=-[]\\\';,./{}|\":<>?~_";  
+
+  if(passWord.value.match(lowerCaseLetters)) { 
+    letter.classList.remove("invalid");
+    letter.classList.add("valid");
+  } else {
+    letter.classList.remove("valid");
+    letter.classList.add("invalid");
+}
+  if(passWord.value.match(upperCaseLetters)) { 
+    capital.classList.remove("invalid");
+    capital.classList.add("valid");
+  } else {
+    capital.classList.remove("valid");
+    capital.classList.add("invalid");
+}
+
+  if(passWord.value.match(numbers)) { 
+    number.classList.remove("invalid");
+    number.classList.add("valid");
+  } else {
+    number.classList.remove("valid");
+    number.classList.add("invalid");
+}
+  if(passWord.value.match(sChars)) { 
+    splChars.classList.remove("invalid");
+    splChars.classList.add("valid");
+  } else {
+    splChars.classList.remove("valid");
+    splChars.classList.add("invalid");
+}
+
+   // Validate length
+  if(passWord.value.length >= 6) {
+    length.classList.remove("invalid");
+    length.classList.add("valid");
+  } else {
+    length.classList.remove("valid");
+    length.classList.add("invalid");
+}
+}
 
 
 
--- routes/index.js
+++ routes/index.js
@@ -20,8 +20,9 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/login', function(req, res, next) {
-  var userNumber = req.body.number;
-  var userPassword = req.body.pass;
+  var userName = req.body.userName;
+  var userPassword = req.body.passWord;
+  
   var options = { method: 'GET',
     url: 'https://www.pay2all.in/web-api/get-provider',
     qs: { api_token: '1swdyd5JddEUDK8iqwZJpMmCTPzakBemqOIAwV00f1O9x0LDG5hQjtb98brW' }
@@ -37,8 +38,11 @@ router.post('/login', function(req, res, next) {
 });
 
 router.post('/signUp', function(req, res, next) {
-  var userNumber = req.body.number;
-  var userPassword = req.body.pass;
+  var userName = req.body.userName;
+  var userEmailId = req.body.emailId;
+  var userNumber = req.body.mobileNumber;
+  var userPassword = req.body.passWord;
+  
   var options = { method: 'GET',
     url: 'https://www.pay2all.in/web-api/get-provider',
     qs: { api_token: '1swdyd5JddEUDK8iqwZJpMmCTPzakBemqOIAwV00f1O9x0LDG5hQjtb98brW' }
