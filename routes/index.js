82a114f66e100bb8d8dfe732313590b8008c5735
himanshu <himanshu.yadav@sendinblue.com>
2018-01-21 15:22:36 +0530

    fix: lapsed data pushed

--- routes/index.js
+++ routes/index.js
@@ -1,55 +1,65 @@
-var express = require('express');
-var router = express.Router();
-var request = require('request')
+const express = require('express');
+
+const router = express.Router();
+const request = require('request')
 const http = require('http')
+
 const httpsAgent = new http.Agent({ keepAlive: true });
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-	var options = { method: 'GET',
-  	url: 'https://www.pay2all.in/web-api/get-provider',
-  	qs: { api_token: '1swdyd5JddEUDK8iqwZJpMmCTPzakBemqOIAwV00f1O9x0LDG5hQjtb98brW' }
-  	 };
-
-	request(options, function (error, response, body) {
-  		if (error) throw new Error(error);
-  		body = JSON.parse(body)
-  		console.log(body.providers);
-  		res.render('index', {data:body});
-	});
-});
-
-router.post('/login', function(req, res, next) {
-  var userNumber = req.body.number;
-  var userPassword = req.body.pass;
-  var options = { method: 'GET',
+router.get('/', (req, res) => {
+  const options = {
+    method: 'GET',
     url: 'https://www.pay2all.in/web-api/get-provider',
-    qs: { api_token: '1swdyd5JddEUDK8iqwZJpMmCTPzakBemqOIAwV00f1O9x0LDG5hQjtb98brW' }
-   };
-  
-
-  request(options, function (error, response, body) {
-      if (error) throw new Error(error);
-      body = JSON.parse(body)
-      console.log(body.providers);
-      res.render('index', {data:body});
+    qs: {
+      api_token: '1swdyd5JddEUDK8iqwZJpMmCTPzakBemqOIAwV00f1O9x0LDG5hQjtb98brW'
+    },
+    agent: httpsAgent
+  };
+  request(options, (error, response, body) => {
+    if (error) throw new Error(error);
+    const bodyData = JSON.parse(body)
+    console.log(bodyData.providers)
+    res.render('index', { data: body });
   });
 });
 
-router.post('/signUp', function(req, res, next) {
-  var userNumber = req.body.number;
-  var userPassword = req.body.pass;
-  var options = { method: 'GET',
+router.post('/login', (req, res) => {
+  const userNumber = req.body.number;
+  const userPassword = req.body.pass;
+  const options = {
+    method: 'GET',
+    url: 'https://www.pay2all.in/web-api/get-provider',
+    qs: {
+      api_token: '1swdyd5JddEUDK8iqwZJpMmCTPzakBemqOIAwV00f1O9x0LDG5hQjtb98brW'
+    },
+    agent: httpsAgent
+  };
+
+
+  request(options, (error, response, body) => {
+    if (error) throw new Error(error);
+    const bodyData = JSON.parse(body)
+    console.log(body.providers);
+    res.render('index', { data: bodyData });
+  });
+});
+
+router.post('/signUp', (req, res) => {
+  const userNumber = req.body.number;
+  const userPassword = req.body.pass;
+  const options = {
+    method: 'GET',
     url: 'https://www.pay2all.in/web-api/get-provider',
     qs: { api_token: '1swdyd5JddEUDK8iqwZJpMmCTPzakBemqOIAwV00f1O9x0LDG5hQjtb98brW' }
-   };
-  
+  };
 
-  request(options, function (error, response, body) {
-      if (error) throw new Error(error);
-      body = JSON.parse(body)
-      console.log(body.providers);
-      res.render('index', {data:body});
+
+  request(options, (error, response, body) => {
+    if (error) throw new Error(error);
+    const bodyData = JSON.parse(body)
+    console.log(bodyData.providers);
+    res.render('index', { data: body });
   });
 });
 
